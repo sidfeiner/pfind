b36e8c3c523f2e352c21efdb3e97ec7fb81f24b9
sidney.feiner <sidney.feiner@startapp.com>
2020-12-26 15:02:27 +0200

    improve permissions

--- pfind.c
+++ pfind.c
@@ -267,9 +267,11 @@ char *pathJoin(char *dir, char *entry) {
 }
 
 int hasReadPermission(char *path) {
-    struct stat s;
-    lstat(path, &s);
-    return s.st_mode & S_IRUSR;
+    struct stat fileStat;
+    if (lstat(path, &fileStat)) {
+        return 0;
+    }
+    return fileStat.st_mode & S_IRUSR;
 }
 
 /**
